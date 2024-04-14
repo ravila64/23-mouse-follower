096853b8c7ddc762f73a12678dfb6a7246629c65
Rene Avila <rene.avila.alonso@gmail.com>
2024-04-13 21:29:44 -0500

    hasta act/desact cursorcon otro effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,71 @@
-import { useEffect, useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
 // alt+96=` comillas invertidas
 
-export function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  // pointer move
   useEffect(() => {
     console.log("effect ", { enabled });
+
     const handleMove = (event) => {
       const { clientX, clientY } = event;
-      console.log('handleMove ', { clientX, clientY });
+      console.log("handleMove ", { clientX, clientY });
       setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
     }
-    if (enabled) { window.addEventListener('pointermove', handleMove); }
     // cleanup, cuando el componente se desmonta
     // cuando cambian las dependencias antes de ejecutar el efecto
-    return ()=>{
-      window.removeEventListener('pointermove',handleMove);
-    }
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el compo
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
   }, [enabled]);
 
   return (
-    <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: `translate(${position.x}px,${position.y}px)`
-      }}>
-      </div>
-      <h3>proyecto 3 de react midudev</h3>
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar ' : 'Activar '}
+        {enabled ? "Desactivar " : "Activar "}
         Seguir puntero
       </button>
+    </>
+  );
+};
+
+export function App() {
+  // const [mounted,setMounted] = useState(true);
+  return (
+    <main>
+      <FollowMouse />
     </main>
   );
 }
